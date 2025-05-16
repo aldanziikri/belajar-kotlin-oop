18d08588a504ad6fbc1c2f74015c57bee3085f39
aldanziikri <aldanzikri23398@gmail.com>
2025-05-16 22:18:41 +0700

    properties di constructor

--- src/app/app.kt
+++ src/app/app.kt
@@ -5,7 +5,6 @@ import data.Person.Person
 
 fun main() {
 val aldan = Person("aldan", "zikriiiii")
-    println(aldan.fistName)
-    println(aldan.middleName)
-    println(aldan.lastName)
+    println(aldan.firstname)
+    println(aldan.lastname)
 }
--- src/data/Person.kt
+++ src/data/Person.kt
@@ -1,13 +1,7 @@
 package data.Person
 
-class Person(firstname: String = "Tidak ada nama", lastname: String = "") {
+class Person(var firstname: String = "Tidak ada nama", var lastname: String = "") {
 
-    var fistName: String = firstname
-    var middleName: String = ""
-    var lastName: String = lastname
 
-    constructor(firstName: String, midName:String, lastName:String) : this(firstName, lastName) {
-        middleName = midName
-    }
 }
 
