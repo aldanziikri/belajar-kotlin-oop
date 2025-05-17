b69b1c837a4d120f8039d0dd36320c5da41a5363
aldanziikri <aldanzikri23398@gmail.com>
2025-05-17 20:41:36 +0700

    inharitence

--- src/app/app.kt
+++ src/app/app.kt
@@ -1,10 +1,15 @@
 package app
 
 import data.Person.Person
+import data.Person.Siswa
 
 
 fun main() {
-val aldan = Person("aldan")
-    aldan.sayHello()
-    aldan.sayHello("zuma")
+    val aldan = Person("aldan")
+        aldan.sayHello("zuma")
+
+    val siswa = Siswa("goku")
+    siswa.sayHello("zumaaa")
+//    println(siswa.nametwo)
+//    println(siswa.name)
 }
--- src/data/Person.kt
+++ src/data/Person.kt
@@ -1,12 +1,14 @@
 package data.Person
 
-class Person(var name: String = "Tidak ada nama") {
+open class Person(var name: String = "Tidak ada nama") {
 
-    fun sayHello() {
-        println("Hello ${this.name}")
-    }
     fun sayHello(name:String) {
         println("Hello $name, nama saya ${this.name}")
     }
 }
+
+class Siswa(name: String) :  Person(name){
+
+}
+
 
